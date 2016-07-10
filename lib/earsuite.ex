92b05e4e6b031daa55ed060a9cabe4e00af494c9
RobertDober <robert.dober@gmail.com>
2016-07-10 07:37:08 +0200

    comitting before next spec creations (to be able to undo them with git checkout)

--- lib/earsuite.ex
+++ lib/earsuite.ex
@@ -1,8 +1,28 @@
 defmodule Earsuite do
 
+  import Earsuite.Tools, only: [emit_colorized: 1, associate_html_file: 1, find_spec_pairs: 1]
+
   @doc """
   """
   @spec make_specs( String.t() ) :: boolean()
   def make_specs(from_path) do
+    from_path
+    |> find_spec_pairs() 
+    |> Stream.filter(fn {_,x} -> !x end)
+    |> Stream.map(&make_spec/1)
+    |> Stream.run()
+  end
+
+  defp generate_html_file(md_file, html_file) do 
+    with html = md_file |> File.read!() |> Earmark.to_html() do
+      File.write!(html_file, html)
+      emit_colorized(["Html spec ", :green, html_file, :reset, " created"])
+    end
+  end
+
+  defp make_spec {md_file, false} do 
+    with html_file = md_file |> associate_html_file(),
+    do:
+      generate_html_file(md_file, html_file)
   end
 end
--- lib/earsuite/tools.ex
+++ lib/earsuite/tools.ex
@@ -3,40 +3,51 @@ defmodule Earsuite.Tools do
     @default_spec_dir "specs"
     @markdown_rgx     ~r{\.md$}
 
+    def emit_colorized ansi_and_bins do
+      ansi_and_bins
+      |> IO.ANSI.format(true)
+      |> IO.iodata_to_binary()
+      |> IO.puts()
+
+    end
     @doc """
       Find files in the `dir` directory that match the `filter` regexp (only the file's basename, not
       the whole path, is matched). Recurse into subdirs unless `recursive` is `false`. Defaults to `true`.
     """
-    def find_files_in_dir(dir, filter, recursive \\ true) do 
+    def find_files_in_dir(dir, filter, recursive \\ true) do
       with dir    = dir |> to_char_list(),
            filter = filter |> Regex.source() |> to_char_list(),
        do:
-         :filelib.fold_files( dir, filter, recursive, fn x, a -> [to_string(x)|a] end, [] ) 
+         :filelib.fold_files( dir, filter, recursive, fn x, a -> [to_string(x)|a] end, [] )
          |> Stream.map(&(&1))
     end
 
     @doc """
     Find all files matching `~r{\.md$}` recursively in `dir`
     """
-    def find_md_files(dir \\ @default_spec_dir) do 
+    def find_md_files(dir \\ @default_spec_dir) do
       find_files_in_dir(dir, @markdown_rgx)
     end
 
     @doc """
     Associate files to a stream of files
     """
-    def associate_files(file_stream, assoc_fun) do 
+    def associate_files(file_stream, assoc_fun) do
       Stream.map(file_stream, assoc_fun)
     end
 
-    def find_spec_pairs(dir \\ @default_spec_dir) do 
+    def associate_html_file(md_file) do
+      Regex.replace(@markdown_rgx, md_file, ".html")
+    end
+
+    def find_spec_pairs(dir \\ @default_spec_dir) do
       find_md_files(dir)
       |> associate_files(&make_spec_pair/1)
     end
 
-    def make_spec_pair(file) do 
-      with alt_file = Regex.replace(@markdown_rgx, file, ".html"), do:
-        {file, File.exists?(alt_file) && alt_file} 
+    def make_spec_pair(file) do
+      with alt_file = file |> associate_html_file(),
+      do:
+        {file, File.exists?(alt_file) && alt_file}
     end
-  
 end
